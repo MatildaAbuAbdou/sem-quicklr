c58a18fa3ca1c226b61ef71b1fcf23626bb5d35a
Matilda AbuAbdou <mabuabdou@expedia.com>
2019-01-14 13:06:52 +0200

    Initial code

---
+++ src/main/java/com/testandlearn/quicklr/QuicklrApplication.java
@@ -0,0 +1,14 @@
+package com.testandlearn.quicklr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuicklrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuicklrApplication.class, args);
+	}
+
+}
+
